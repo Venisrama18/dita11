2a23cf7d1f1cb9c535b48be54eca51fea088a3d5
Venisrama18 <venisrama7@gmail.com>
2025-03-22 10:52:18 +0100

    login and register

---
+++ signup.php
@@ -0,0 +1,48 @@
+<?php include_once 'footer.php'; ?>
+
+<div class="container">
+    <div class="w-50 mx-auto " style="margin: 100px auto;">
+        <h1 class="mb-5">Please sign in </h1>
+
+        <form>
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="form2Example3" class="form-control" />
+                <label class="form-label" for="form2Example3">Name</label>
+            </div>
+
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="form2Example4" class="form-control" />
+                <label class="form-label" for="form2Example4">Surname</label>
+            </div>
+
+            <!-- Email input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="form2Example5" class="form-control" />
+                <label class="form-label" for="form2Example5">Username</label>
+            </div>
+
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="form2Example3" class="form-control" />
+                <label class="form-label" for="form2Example3">Email address</label>
+            </div>
+
+            <!-- Password input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="password" id="form2Example2" class="form-control" />
+                <label class="form-label" for="form2Example2">Password</label>
+            </div>
+
+            <!-- Submit button -->
+            <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+
+            <!-- Register buttons -->
+            <div class="text-center">
+                <p>Have an account <a href="#!">sign in </a></p>
+            </div>
+
+        </form>
+
+        </body>
+    </div>
+</div>
+<?php include_once 'footer.php'; ?>
